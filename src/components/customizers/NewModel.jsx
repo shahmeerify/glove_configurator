3a123aa5212ecad0a4386dd81314a0d3eb01f6d5
Muhammad Shahmeer <24100237@lums.edu.pk>
2023-11-15 00:49:44 +0500

    Texture Size Changed

--- src/components/customizers/NewModel.jsx
+++ src/components/customizers/NewModel.jsx
@@ -6,7 +6,7 @@ import * as THREE from 'three';
 
 const MeshWithTexture = ({ geometry, material, color, position, rotation, scale, texture }) => {
   const textureMap = useTexture(texture);
-  textureMap.repeat.set(6, 6)
+  textureMap.repeat.set(7, 7)
   textureMap.wrapS = THREE.RepeatWrapping 
   textureMap.wrapT = THREE.RepeatWrapping
   return (
